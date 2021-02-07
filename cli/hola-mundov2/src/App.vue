5aa732e9b7f481bc9002f8fd8cb2912a1a887be4
Sebas <seplarui@gmail.com>
2021-02-07 19:50:04 +0100

    cuenta2

--- cli/hola-mundov2/src/App.vue
+++ cli/hola-mundov2/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Titulo texto="Mi banco dinámico 2.0" />
+    <Cuenta/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Titulo from "./components/Titulo";
+import Cuenta from "./components/Cuenta";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Titulo,
+    Cuenta,
+  },
+};
 </script>
 
 <style>
